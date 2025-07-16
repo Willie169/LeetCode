0b1dcffe4286af4976a5b862e9fd1b4cc156fbc5
Wiilie169 <cg10250207@gmail.com>
2025-07-16 16:19:48 +0800

    m

--- 1TwoSum.cpp
+++ 1TwoSum.cpp
@@ -1,32 +1,18 @@
 class Solution
 {
-  public:
-	vector<int> twoSum(vector<int> &nums, int target)
-	{
-		unordered_map<int,int> m;
-		vector<int> r;
-		for (int i=0; i<nums.size(); i++)
-		{
-			m.insert({nums[i],i});
-		}
-		int *s=&nums[0];
-		int *e=&nums[nums.size()-1];
-		while (1)
-		{
-			if (s[0]+e[0]==target)
-			{
-				r.push_back(m[s[0]]);
-				r.push_back(m[e[0]]);
-				return r;
-			}
-			else if (s[0]+e[0]<target)
-			{
-				s=&s[1];
-			}
-			else
-			{
-				e=&e[-1];
-			}
-		}
-	}
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int c = target - nums[i];
+            if (m.count(c))
+            {
+                return {m[c], i};
+            }
+            m[nums[i]] = i;
+        }
+        return {};
+    }
 };
