d1ac5c83843cdf170398794b3a721cb88e52ec1e
Wiilie169 <cg10250207@gmail.com>
2025-07-22 18:18:19 +0800

    ,

--- 242ValidAnagram.cpp
+++ 242ValidAnagram.cpp
@@ -3,10 +3,10 @@ public:
     bool isAnagram(string s, string t) {
         if (s.size()!=t.size()) return false;
         unsigned short arr[26] = {0};
-        for (auto i=s.begin(); i<s.end(); i++) {
+        for (auto i=s.begin(); i!=s.end(); i++) {
             arr[*i-'a']++;
         }
-        for (auto i=t.begin(); i<t.end(); i++) {
+        for (auto i=t.begin(); i!=t.end(); i++) {
             if (arr[*i-'a']<1) return false;
             arr[*i-'a']--;
         }
